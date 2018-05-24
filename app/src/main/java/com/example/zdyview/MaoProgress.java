ccd63d023d689c1f2438cf0eac8428c6fc2e2eb0
“your_username” <67698619@qq.com>
2018-05-24 17:30:54 +0800

    简单增加

---
+++ app/src/main/java/com/example/zdyview/MaoProgress.java
@@ -0,0 +1,35 @@
+package com.example.zdyview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.widget.ProgressBar;
+
+/**
+ * Created by 67698 on 2018/5/24.
+ */
+
+public class MaoProgress extends ProgressBar {
+    public MaoProgress(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MaoProgress(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected synchronized void onDraw(Canvas canvas) {
+
+    }
+    public int dp2px(int dp)//dp转px
+    {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,dp,getResources().getDisplayMetrics());
+    }
+
+    public int sp2px(int sp)//sp转px
+    {
+        return (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,sp,getResources().getDisplayMetrics());
+    }
+}
